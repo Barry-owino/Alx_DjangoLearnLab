2848ec860665b0ce58344f3b296e16ed29a799cd
Barry-owino <barryowino5@gmail.com>
2025-03-04 14:40:28 +0300

    fixed another error

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,7 +8,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 #query all books by a specific author
 def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name).first()
+    author = Author.objects.filter(name=author_name).first()
     if author:
         books = author.books.all()
         return [book.title for book in books]
@@ -16,7 +16,7 @@ def get_books_by_author(author_name):
 
 #list all books in a library
 def get_books_in_library(library_name):
-    library = Library.objects.get(name=library_name).first()
+    library = Library.objects.filter(name=library_name).first()
     if library:
         books = library.books.all()
         return [book.title for book in books]
