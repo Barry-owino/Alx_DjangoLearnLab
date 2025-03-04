6ae9365dec726b5b747ee249b75ada9cfa204b2d
Barry-owino <barryowino5@gmail.com>
2025-03-04 18:05:20 +0300

    checker corrections

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book
-from .models import Library
+from .models import Book, Library
 
 # Create your views here.
 
