662c5678fb321d1b2405fad7b32fac6a8e0b3326
Barry-owino <barryowino5@gmail.com>
2025-03-04 14:37:17 +0300

    fixing errors

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,7 +8,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 #query all books by a specific author
 def get_books_by_author(author_name):
-    author = Author.objects.filter(name=author_name).first()
+    author = Author.objects.get(name=author_name).first()
     if author:
         books = author.books.all()
         return [book.title for book in books]
@@ -16,14 +16,14 @@ def get_books_by_author(author_name):
 
 #list all books in a library
 def get_books_in_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
+    library = Library.objects.get(name=library_name).first()
     if library:
         books = library.books.all()
         return [book.title for book in books]
     return []
 
 def get_librarian_for_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
+    library = Library.objects.get(name=library_name).first()
     if library and hasattr(library, "librarian"):
         return library.librarian.name
     return None
@@ -33,6 +33,5 @@ if __name__ == "__main__":
     print(get_books_by_author("J.K. Rawlings"))
     print(get_books_in_library("Central Library"))
     print(get_librarian_for_library("Central Library"))
-    print
 
 
