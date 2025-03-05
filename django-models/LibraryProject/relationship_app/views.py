9f0f674a45172a62b733d099471e8a45f9a74cf4
Barry-owino <barryowino5@gmail.com>
2025-03-05 11:26:38 +0300

    created login, logout, register,and html form templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LogoutView
+
 from .models import Book
 from .models import Library
 
+
 # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+
+    else:
+       form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
 
 def list_books(request):
     books = Book.objects.all()
@@ -14,4 +34,10 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = '/login/'
 
+class CustomLogoutView(LogoutView):
+    template_name = 'registration/logout.html'
