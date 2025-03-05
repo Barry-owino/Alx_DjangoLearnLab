9707fbb68aad0bcb393356911d17f7c5c103ae5b
Barry-owino <barryowino5@gmail.com>
2025-03-05 23:11:17 +0300

    checker error

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -44,8 +44,9 @@ def add_book(request):
             return redirect('book_list')
     else:
         form = BookForm()
-return render(request, 'add_book.html', {'form': form})
+return render(request, 'add_book.html', {'form': form})"""
 """
+
 @permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
     if request.method == 'POST':
