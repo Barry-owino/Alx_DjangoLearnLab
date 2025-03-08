50d61d44239926105bd88481ba70d7e958fb3058
Barry-owino <barryowino5@gmail.com>
2025-03-08 12:47:41 +0300

    integrate custom user model into admin

--- advanced_features_and_security/LibraryProject/accounts/admin.py
+++ advanced_features_and_security/LibraryProject/accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'date_of_birth', 'profile_photo', 'email', 'is_staff', 'is_active']
+    list_filter = ['is_staff', 'is_active', 'date_of_birth']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'date_of_birth', 'profile_photo', 'is_active', 'is_staff'),
+        }),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+# Register the CustomUser model with the custom admin interface
+admin.site.register(CustomUser, CustomUserAdmin)
+
