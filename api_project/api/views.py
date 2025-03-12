2f435d118aebaefc3f9b49b85e9de5e7abf56285
Barry-owino <barryowino5@gmail.com>
2025-03-12 10:51:55 +0300

    added permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
 #from django.shortcuts import render
 #from rest_framework import generics
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
 
@@ -8,3 +8,4 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAdminUser] #only admins can modify
