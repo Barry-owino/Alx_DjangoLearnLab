719752164c2e950e2c41bc872074485ab647b169
Barry-owino <barryowino5@gmail.com>
2025-03-14 15:57:21 +0300

    added the import djnago_filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Book
 from .serializers import BookSerializer
