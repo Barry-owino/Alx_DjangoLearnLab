04296d637b0af890b6043b94235a7eb00d1965fe
Barry-owino <barryowino5@gmail.com>
2025-03-14 16:09:37 +0300

    correct import filter

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,8 @@ from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import rest_framework
 from rest_framework import filters
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.filters import SearchFilte
+from rest_framework.filters import OrderingFilter
 from .models import Book
 from .serializers import BookSerializer
 
