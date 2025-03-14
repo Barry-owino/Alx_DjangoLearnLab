bbe7c1efc6d1e3510f4adfa8b0016ba6124153db
Barry-owino <barryowino5@gmail.com>
2025-03-14 16:30:39 +0300

    added the filter import

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework
 from .models import Book, Author
 from .serializers import BookSerializer
 from datetime import datetime
