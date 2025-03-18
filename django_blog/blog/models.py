461769f46ca5fbee868af71453add975a1cfaf5d
Barry-owino <barryowino5@gmail.com>
2025-03-18 14:09:01 +0300

    created the comment model,makemigrations and migrate

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -18,6 +18,20 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     profile_picture = models.ImageField(upload_to='profile/', blank=True)
 
+#comment models
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post.title}"
+
+    class Meta:
+        ordering = ['-created_at']
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
