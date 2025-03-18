bd32dac959ccde56bcc7b17a491bf5c3a616715d
Barry-owino <barryowino5@gmail.com>
2025-03-18 19:45:30 +0300

    added url config for search and tag

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -25,4 +25,8 @@ urlpatterns = [
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+
+    #url config for tag and search
+    path('tags/<slug:slug>/', TaggedPostView.as_view(), name='tagged-posts'),
+    path('search/', SearchResultsView.as_view(), name='search-result'),
 ]
