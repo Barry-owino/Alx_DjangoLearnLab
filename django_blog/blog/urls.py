98f5520394ed350fa38a7111e586194a60d63815
Barry-owino <barryowino5@gmail.com>
2025-03-18 14:56:06 +0300

    added the url configs

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -20,4 +20,9 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    #url config for comments
+    path('post/<int:post_id/comment/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
