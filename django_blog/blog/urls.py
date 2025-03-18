05c0ca54ea51a9982a278ff4631e89896ffbb339
Barry-owino <barryowino5@gmail.com>
2025-03-18 15:10:22 +0300

    correction on the urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
 
     #url config for comments
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
