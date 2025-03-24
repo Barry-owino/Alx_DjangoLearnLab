7e7aaadf7a5f65109602ec489f0b677ece9ede72
Barry-owino <barryowino5@gmail.com>
2025-03-24 22:01:45 +0300

    created views for user registration and authentication

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
