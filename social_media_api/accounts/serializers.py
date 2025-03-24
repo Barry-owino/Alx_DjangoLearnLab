8b5d27ead12a832e46b58dbaa447e3948f6edfb7
Barry-owino <barryowino5@gmail.com>
2025-03-24 22:11:33 +0300

    created the serializers correctly for the checker

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,34 +1,28 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-from django.contrib.auth import authenticate
-from .models import User
+User = get_user_model()
 
-class UserRegistrationSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
+
+class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'bio', 'profile_picture')
+        fields = ['id', 'username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
+        Token.objects.create(user=user)  # Ensure token is generated upon registration
         return user
 
-class UserLoginSerializer(serializers.Serializer):
+class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
-
-    def validate(self, data):
-        user = authenticate(**data)
-        if user and user.is_active:
-            return user
-        raise serializers.ValidationError("Incorrect Credentials")
-
-class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'bio', 'profile_picture', 'followers')
 
 
