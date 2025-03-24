9bde05b3d792100d17f963417db2fb9056bf7b86
Barry-owino <barryowino5@gmail.com>
2025-03-24 18:00:09 +0300

    Fix model conflicts by updating related_name for groups and user_permissions in CustomUser model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="customuser_set",  # added this to avoid conflicts
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="customuser_permissions_set",  # added this to avoid conflicts
+        blank=True
+    )
+
+
+    def __str__(self):
+        return self.username
