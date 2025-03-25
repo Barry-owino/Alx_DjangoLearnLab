fe1820b3d0e21c25c50f7a239913adc9a65960ea
Barry-owino <barryowino5@gmail.com>
2025-03-25 20:42:10 +0300

    added following field to the user model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -6,18 +6,9 @@ from django.db import models
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
-
-    groups = models.ManyToManyField(
-        "auth.Group",
-        related_name="customuser_set",  # added this to avoid conflicts
-        blank=True
-    )
-    user_permissions = models.ManyToManyField(
-        "auth.Permission",
-        related_name="customuser_permissions_set",  # added this to avoid conflicts
-        blank=True
-    )
+    groups = models.ManyToManyField( "auth.Group", related_name="customuser_set", blank=True )
+    user_permissions = models.ManyToManyField( "auth.Permission", related_name="customuser_permissions_set", blank=True )
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
 
 
     def __str__(self):
