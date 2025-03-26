9c4c3f7f3f9365bc9043c76f86c5420063096cc2
Barry-owino <barryowino5@gmail.com>
2025-03-26 19:32:04 +0300

    added route for feed endpoint in the posts urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, FeedView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -9,5 +9,6 @@ router.register(r'comments', CommentViewSet)
 # Include router URLs
 urlpatterns = [
     path('', include(router.urls)),
+    path('feed/', FeedView.as_view(), name='feed'),
 ]
 
