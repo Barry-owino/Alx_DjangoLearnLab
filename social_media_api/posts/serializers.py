be83eaccabf083282d187f8b7e71e15a8d95068b
Barry-owino <barryowino5@gmail.com>
2025-03-26 20:22:17 +0300

    created the LikeSerializer inside post serializers.py

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post, Comment
+from .models import Post, Comment, Like
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
@@ -15,3 +15,7 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = ['id', 'author', 'post', 'content', 'created_at', 'updated_at']
 
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ['id', 'user', 'post', 'created_at']
