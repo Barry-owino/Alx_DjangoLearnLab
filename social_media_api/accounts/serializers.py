d1a642ba8f84978d6d201934df440aede370753c
Barry-owino <barryowino5@gmail.com>
2025-03-27 13:42:35 +0300

    created the serializers

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password', 'bio', 'profile_picture')
+        extra_kwargs = {'password': {'write_only':True}}
+
+        def create(self, validated_data):
+            user = CustomUser.objects.create_user(
+                username=validated_data['username'],
+                email=validated_data['email'],
+                password=validate_data['password'],
+                bio=validated_data.get('bio', ''),
+                profile_picture=validated_data.get('profile_picture', None)
+            )
+            return user
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
+        read_only_fields = ('id', 'username', 'email', 'followers')
