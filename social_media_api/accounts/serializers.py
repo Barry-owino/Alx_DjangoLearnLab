f7c9ea5d02d34c10c1bda4049f5a5d3c0471c409
Barry-owino <barryowino5@gmail.com>
2025-03-27 14:47:31 +0300

    fixed indent and typo in the serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -10,20 +10,23 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
-        extra_kwargs = {'password': {'write_only':True}}
+        extra_kwargs = {'password': {'write_only': True}}
 
-        def create(self, validated_data):
-            user = User.objects.create_user(
-                username=validated_data['username'],
-                email=validated_data['email'],
-                password=validate_data['password'],
-                bio=validated_data.get('bio', ''),
-                profile_picture=validated_data.get('profile_picture', None)
-            )
-            return user
+    def create(self, validated_data):  
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        # Generate authentication token
+        token, _ = Token.objects.get_or_create(user=user)
+        return user
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
         read_only_fields = ('id', 'username', 'email', 'followers')
+
