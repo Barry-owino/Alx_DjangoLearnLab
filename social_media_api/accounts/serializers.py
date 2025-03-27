48b9b3535e1221f5a7b2f4b259cb3479b7f110c6
Barry-owino <barryowino5@gmail.com>
2025-03-27 15:06:14 +0300

    updated the srializers.py for the checker error

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,6 +5,8 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    username = serializers.CharField()
+    email = serializers.CharField() 
     password = serializers.CharField(write_only=True, style={'input_type': 'password'})
 
     class Meta:
@@ -13,14 +15,13 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
-
         Token.objects.create(user=user)
         return user
 
@@ -28,5 +29,6 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
-        read_only_fields = ('id', 'username', 'email', 'followers') 
+        read_only_fields = ('id', 'username', 'email', 'followers')
+
 
