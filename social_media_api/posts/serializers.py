f33df03a19a11852d051cd865fda664df49d6b77
Barry-owino <barryowino5@gmail.com>
2025-03-27 19:28:37 +0300

    created serializer to hundle post and comments

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Post, Comment
+from django.contrib.auth import grt_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'content', 'author', 'created_at', 'updated_at']
+        read_only_fields = ['author', 'created_at', 'updated_at']
+
+class PostSerializer(serializers.ModelSerializer):
+    author = UseSerializer(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'author', 'created_at', 'updated_at', 'comments']
+        read_only_fields = ['author', 'created_at', 'updated_at']
