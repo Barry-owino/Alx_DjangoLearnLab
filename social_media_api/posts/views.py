f00b37ec21dda18908a604fef9c75affcf26de17
Barry-owino <barryowino5@gmail.com>
2025-03-27 19:51:37 +0300

    created views to manage comments and post and authentications

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+class IsOwnerReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnwerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_created(self, serializer):
+        serializer.save(author=self.request.user)
+
