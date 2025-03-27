9fb68235f1a7202c435b5efc2054825344daf6df
Barry-owino <barryowino5@gmail.com>
2025-03-27 14:58:57 +0300

    correcte missing token import in serialiazers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -12,7 +12,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):  
+    def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
@@ -20,13 +20,13 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
-        # Generate authentication token
-        token, _ = Token.objects.get_or_create(user=user)
+
+        Token.objects.create(user=user)
         return user
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
-        read_only_fields = ('id', 'username', 'email', 'followers')
+        read_only_fields = ('id', 'username', 'email', 'followers') 
 
