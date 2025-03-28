5d030dd58a0053261027b12bcd57e3608eff173d
Barry-owino <barryowino5@gmail.com>
2025-03-28 11:15:55 +0300

    Add user following functionality to customUser model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -5,7 +5,19 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
-    followers = models.ManyToManyField('self', symmetrical=False, blank=True)
-    
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
+     
+    def follow(self, user):
+        if user != self:
+            self.following.add(user)
+
+    def unfollow(self, user):
+        self.following.remove(user)
+
+
+    def is_folloeing(self, user):
+        return self.following.filter(id=user.id).exists()
+
+
     def __str__(self):
         return self.username
