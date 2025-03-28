8aab0c92728c7ac291f414a8ed03f38176d8d778
Barry-owino <barryowino5@gmail.com>
2025-03-28 11:41:06 +0300

    fixed checker error for queryset definition

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -40,11 +40,11 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
         return self.request.user
 
 class FollowUserView(generics.GenericAPIView):
-    """Handles following another user."""
+    queryset = CustomUser.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+        user_to_follow = get_object_or_404(self.get_queryset(), id=user_id)
 
         if request.user.is_following(user_to_follow):
             return Response({'detail': 'You are already following this user.'}, status=status.HTTP_400_BAD_REQUEST)
@@ -54,10 +54,11 @@ class FollowUserView(generics.GenericAPIView):
 
 class UnfollowUserView(generics.GenericAPIView):
     """Handles unfollowing a user."""
+    queryset = CustomUser.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+        user_to_unfollow = get_object_or_404(self.get_queryset(), id=user_id)
 
         if not request.user.is_following(user_to_unfollow):
             return Response({'detail': 'You are not following this user.'}, status=status.HTTP_400_BAD_REQUEST)
