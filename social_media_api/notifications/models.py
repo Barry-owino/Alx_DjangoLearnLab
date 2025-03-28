edc75de96ae9eae9e8ab254e3e7accd5b0193103
Barry-owino <barryowino5@gmail.com>
2025-03-28 14:10:00 +0300

    added timestamp to the notification model

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -14,6 +14,7 @@ class Notification(models.Model):
     target = GenericForeignKey('target_content_type', 'target_object_id')
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.actor.username} {self.verb}"
